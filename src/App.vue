14099011a97ea13bc82ce05b5ae901283fa36628
Brandon Wong <brandonwongck@gmail.com>
2023-05-21 13:56:03 +0800

    Add sideNavBar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import SideNavBar from './components/SideNavBar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <main class="bg-[#151F32]">
+    <SideNavBar />
+  </main>
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -81,5 +69,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
---
+++ src/components/SideNavBar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import IconHome from './icons/IconHome.vue'
+import IconNote from './icons/IconNote.vue'
+import IconLightBulb from './icons/IconLightBulb.vue'
+import IconShield from './icons/IconShield.vue'
+import IconCog from './icons/IconCog.vue'
+</script>
+<template>
+  <nav class="w-[60px] flex flex-col h-[100vh]">
+    <div class="h-5 w-5 p-6 mx-[10px] my-4 rounded-full bg-white self-center" />
+    <div class="grid place-content-center">
+      <div class="p-4 aspect-square hover:bg-[#353D4E] cursor-pointer">
+        <IconHome />
+      </div>
+      <div class="p-4 aspect-square hover:bg-[#353D4E] cursor-pointer">
+        <IconNote />
+      </div>
+      <div class="p-4 aspect-square hover:bg-[#353D4E] cursor-pointer">
+        <IconLightBulb />
+      </div>
+      <div class="p-4 aspect-square hover:bg-[#353D4E] cursor-pointer">
+        <IconShield />
+      </div>
+      <div class="p-4 aspect-square hover:bg-[#353D4E] cursor-pointer">
+        <IconCog />
+      </div>
+    </div>
+  </nav>
+</template>
